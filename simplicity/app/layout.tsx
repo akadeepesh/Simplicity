7b0cceb38517a61c6b33c688ae5df5411d719277
Deepesh Kumar <deepesh.kumar.ug21@nsut.ac.in>
2024-03-20 12:29:08 +0530

    first commit

---
+++ simplicity/app/layout.tsx
@@ -0,0 +1,26 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import ConvexClientProvider from "./ConvexClientProvider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Simplicity",
+  description:
+    "Simplicity is an app that shows poetries that are simple and deserves to be read.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ConvexClientProvider>{children}</ConvexClientProvider>
+      </body>
+    </html>
+  );
+}
