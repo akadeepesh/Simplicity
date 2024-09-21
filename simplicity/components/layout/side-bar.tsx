5741c4e668e8d8e0a8eb2b7bc91cd1bf8c9ba3db
akadeepesh <deepesh.kumar.ug21@nsut.ac.in>
2024-09-21 12:32:59 +0530

    Unsed var

--- simplicity/components/layout/side-bar.tsx
+++ simplicity/components/layout/side-bar.tsx
@@ -25,7 +25,6 @@ import { Link } from "../typography/link";
 import { useUser } from "@clerk/clerk-react";
 import { Authenticated } from "convex/react";
 import { useSelectedLayoutSegments } from "next/navigation";
-import { ModeToggle } from "../theme/mode-toggle";
 
 const SideBar = () => {
   const { user } = useUser();
